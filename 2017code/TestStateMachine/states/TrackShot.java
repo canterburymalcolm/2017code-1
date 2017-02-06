a40715679e443131dcc02600c74a2d64fd56e3b9
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-06 12:57:05 -0800

    Goes to shoot once done tracking

--- 2017code/TestStateMachine/states/TrackShot.java
+++ 2017code/TestStateMachine/states/TrackShot.java
@@ -1,5 +1,24 @@
 package states;
 
-public class TrackShot {
+import org.usfirst.frc.team3328.robot.subsystems.DriveSystem;
+
+import states.StateMachine.States;
+
+public class TrackShot implements RobotState {
+	
+	DriveSystem drive;
+	
+	public TrackShot(DriveSystem dr){
+		drive = dr;
+	}
+	
+	@Override
+	public States run() {
+		drive.track();
+		if (drive.isTracking()){
+			return States.TRACKSHOT;
+		}
+		return States.SHOOT;
+	}
 
 }
