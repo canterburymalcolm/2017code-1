5032f3af608cdb50fbd6c18f88c835e49309cc53
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-06 12:54:32 -0800

    Added gear and track gear to decision tree

--- 2017code/TestStateMachine/states/Move.java
+++ 2017code/TestStateMachine/states/Move.java
@@ -1,34 +1,40 @@
 package states;
 
+import org.usfirst.frc.team3328.robot.subsystems.DriveSystem;
 import states.StateMachine.*;
 
 public class Move implements RobotState{
 	
+	DriveSystem drive;
 	Goals goal;
 	Pos pos;
 	double distance = -1;
 	double distanceTraveled;
 	double iteration = 0;
 	
-	public Move(Goals gl, Pos position){
+	public Move(Goals gl, Pos position, DriveSystem dr){
 		goal = gl;
 		pos = position;
+		drive = dr;
 	}
 	
 	@Override
 	public States run() {
+		distanceTraveled = drive.getDistance();
 		if (distanceTraveled < distance){
-			//move forwards
+			drive.move(.2, .2);
 			return States.MOVE;
 		}
+		drive.move(0, 0);
+		distance = -1;
 		switch (goal){
 			case NOTHING:
 				return States.STOP;
 			case LINE:
-				if (pos == Pos.FL || pos == Pos.FR){
+				if (pos == Pos.FL || pos == Pos.FR || pos == Pos.BL || pos == Pos.BR){
 					return States.STOP;
 				}
-				if (pos == Pos.FM){
+				if (pos == Pos.FM || pos == Pos.BM){
 					if (iteration == 2){
 						return States.TURN;
 					}else{
@@ -37,7 +43,7 @@ public class Move implements RobotState{
 				}
 				break;
 			case SHOOT:
-				if (pos == Pos.FL || pos == Pos.FM){
+				if (pos == Pos.FL || pos == Pos.FM || pos == Pos.BM || pos == Pos.BR){
 					if (iteration == 2 || iteration == 4){
 						return States.TURN;
 					}
@@ -45,12 +51,26 @@ public class Move implements RobotState{
 						return States.TRACKSHOT;
 					}
 				}
-				if (pos == Pos.FR){
+				if (pos == Pos.FR || pos == Pos.BL){
 					if (iteration == 1){
 						return States.TURN;
 					}
 					if (iteration == 3){
 						return States.TRACKSHOT;
+					}
+				}
+				break;
+			case GEAR:
+				if (pos == Pos.FL || pos == Pos.FR || pos == Pos.BL || pos == Pos.BR){
+					if (iteration == 1){
+						return States.TURN;
+					}
+					if (iteration == 3){
+						return States.TRACKGEAR;
+					}
+				}else if(pos == Pos.FM || pos == Pos.BM){
+					if (iteration == 1){
+						return States.TRACKGEAR;
 					}
 				}
 				break;
