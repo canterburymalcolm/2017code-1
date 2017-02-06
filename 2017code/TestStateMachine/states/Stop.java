2450a47d5f2964aceaaab270e113ee8812139a32
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-06 02:37:20 -0800

    created stop state, returns stop when run

---
+++ 2017code/TestStateMachine/states/Stop.java
@@ -0,0 +1,13 @@
+package states;
+
+import states.StateMachine.States;
+
+public class Stop implements RobotState{
+
+	@Override
+	public States run() {
+		// TODO Auto-generated method stub
+		return States.STOP;
+	}
+
+}
