83e4ece8178b19489133b303b92107cc312e13dc
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-06 12:55:39 -0800

    Added gear and trackGear to switch case statement

--- 2017code/TestStateMachine/states/Turn.java
+++ 2017code/TestStateMachine/states/Turn.java
@@ -1,11 +1,16 @@
 package states;
 
+import org.usfirst.frc.team3328.robot.subsystems.DriveSystem;
+import org.usfirst.frc.team3328.robot.utilities.IMU;
+
 import states.StateMachine.Goals;
 import states.StateMachine.Pos;
 import states.StateMachine.States;
 
 public class Turn implements RobotState{
-
+	
+	DriveSystem drive;
+	IMU imu;
 	Goals goal;
 	Pos pos;
 	double range;
@@ -13,12 +18,15 @@ public class Turn implements RobotState{
 	double desired;
 	double iteration = 0;
 	
-	public Turn(Goals gl, Pos position){
+	public Turn(Goals gl, Pos position, DriveSystem dr, IMU ADIS){
 		goal = gl;
 		pos = position;
+		drive = dr;
+		imu = ADIS;
 	}
 	
 	private boolean inRange(){
+		current = imu.getAngleZ();
 		if (Math.abs(desired - current) < range){
 			return true;
 		}
@@ -28,17 +36,19 @@ public class Turn implements RobotState{
 	@Override
 	public States run() {
 		if (!inRange()){
-			//turn
+			drive.autoAngle(0, current, desired);
 			return States.TURN;
 		}
+		drive.move(0, 0);
+		desired = 400;
 		switch (goal){
 			case NOTHING:
 				return States.STOP;
 			case LINE:
-				if (pos == Pos.FL || pos == Pos.FR){
+				if (pos == Pos.FL || pos == Pos.FR || pos == Pos.BL || pos == Pos.BR){
 					return States.STOP;
 				}
-				if (pos == Pos.FM){
+				if (pos == Pos.FM || pos == Pos.BM){
 					if (iteration == 1 || iteration == 3){
 						return States.MOVE;
 					}else{
@@ -47,15 +57,24 @@ public class Turn implements RobotState{
 				}
 				break;
 			case SHOOT:
-				if (pos == Pos.FL || pos == Pos.FM){
+				if (pos == Pos.FL || pos == Pos.FM || pos == Pos.BR || pos == Pos.BM){
 					if (iteration == 1 || iteration == 3 || iteration == 5){
 						return States.MOVE;
 					}
 				}
-				if (pos == Pos.FR){
+				if (pos == Pos.FR || pos == Pos.BL){
 					if (iteration == 2){
 						return States.MOVE;
 					}
+				}
+				break;
+			case GEAR:
+				if (pos == Pos.FL || pos == Pos.FR || pos == Pos.BL || pos == Pos.BR){
+					if (iteration == 2){
+						return States.MOVE;
+					}
+				}else if(pos == Pos.FM || pos == Pos.BM){
+						return States.STOP;
 				}
 				break;
 			default:
