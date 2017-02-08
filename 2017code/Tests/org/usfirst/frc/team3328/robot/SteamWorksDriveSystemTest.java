bcf8373498478e8359fa9603dee5c4e4e0b62f2d
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-08 15:35:44 -0800

    updated constructors

--- 2017code/Tests/org/usfirst/frc/team3328/robot/SteamWorksDriveSystemTest.java
+++ 2017code/Tests/org/usfirst/frc/team3328/robot/SteamWorksDriveSystemTest.java
@@ -6,20 +6,22 @@ import org.usfirst.frc.team3328.robot.networking.Target;
 import org.usfirst.frc.team3328.robot.subsystems.SteamWorksDriveSystem;
 import org.usfirst.frc.team3328.robot.utilities.DriveEncoders;
 import org.usfirst.frc.team3328.robot.utilities.DriveTalons;
+import org.usfirst.frc.team3328.robot.utilities.Tracking;
 
 public class SteamWorksDriveSystemTest {
 	
 	DriveEncoders encoders = new DriveEncoders();
 	FakeController fakeCont1 = new FakeController();
 	FakeController fakeCont2 = new FakeController();
-	Target tar = new Target();
+	Target target = new Target();
+	Tracking track = new Tracking(target, fakeCont2);
 	FakeSpeedController fl = new FakeSpeedController();
 	FakeSpeedController fr = new FakeSpeedController();
 	FakeSpeedController bl = new FakeSpeedController();
 	FakeSpeedController br = new FakeSpeedController();
 	DriveTalons talons = new DriveTalons(fl, fr, bl, br);
 	FakeADIS16448_IMU imu = new FakeADIS16448_IMU();
-	SteamWorksDriveSystem drive = new SteamWorksDriveSystem(encoders, talons, fakeCont1, fakeCont2, tar);
+	SteamWorksDriveSystem drive = new SteamWorksDriveSystem(encoders, talons, fakeCont1, track);
 
 	@Test
 	public void controlledMove_yLargerThanX_rightMotorTurnsBackwards() {
@@ -32,13 +34,14 @@ public class SteamWorksDriveSystemTest {
 	@Test
 	public void updateDisplacement_smallDisplacement_displacementSetToPoint5(){
 		imu.setAngleZ(Math.random() / 20);
-		assertEquals(drive.updateDisplacement(0, imu.getAngleZ()), .05, 0);
+		drive.updateDisplacement(0, imu.getAngleZ());
+		assertEquals(drive.displacement, .05, 0);
 	}
 	
 	@Test
 	public void autoAngle_robotTurnedRight_rightMotorTurnsForwards(){
 		imu.setAngleZ(20);
-		drive.autoAngle(0, imu.getAngleZ(), 0);
+		drive.autoAngle(imu.getAngleZ(), 0);
 		assertTrue(fr.speed < 0 && br.speed < 0);
 		
 	}
