4c77462b83d48a5af1f3ade3389beec3d66efe00
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-08 12:37:26 -0800

    changed constructor for testing purposes

--- 2017code/src/org/usfirst/frc/team3328/robot/networking/NetworkTablesTargetProvider.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/networking/NetworkTablesTargetProvider.java
@@ -1,8 +1,11 @@
 package org.usfirst.frc.team3328.robot.networking;
 
+import edu.wpi.first.wpilibj.SpeedController;
+
 public class NetworkTablesTargetProvider implements TargetProvider{
 	
 	Target target = new Target();
+	
 	
 	//Starts "listenerThread" upon instantiation
 	Listener listener = new Listener("listenerThread", target);
