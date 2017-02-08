6669d5d6fb6f257ea4b73fb8dd138ba440586b49
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-08 15:33:23 -0800

    run method now goes until current == desired

--- 2017code/src/states/Turn.java
+++ 2017code/src/states/Turn.java
@@ -3,24 +3,16 @@ package states;
 import org.usfirst.frc.team3328.robot.subsystems.DriveSystem;
 import org.usfirst.frc.team3328.robot.utilities.IMU;
 
-import states.StateMachine.Goals;
-import states.StateMachine.Pos;
-import states.StateMachine.States;
 
 public class Turn implements RobotState{
 	
 	DriveSystem drive;
 	IMU imu;
-	Goals goal;
-	Pos pos;
 	double range = .5;
 	double current;
-	double desired = 400;
-	double iteration = 0;
+	double desired;
 	
-	public Turn(Goals gl, Pos position, DriveSystem dr, IMU ADIS){
-		goal = gl;
-		pos = position;
+	public Turn(DriveSystem dr, IMU ADIS){
 		drive = dr;
 		imu = ADIS;
 	}
@@ -34,54 +26,20 @@ public class Turn implements RobotState{
 	}
 	
 	@Override
-	public States run() {
-		System.out.println("turning");
-		if (!inRange()){
-			drive.autoAngle(0, current, desired);
-			return States.TURN;
-		}
-		drive.move(0, 0);
-		desired = 400;
-		switch (goal){
-			case NOTHING:
-				return States.STOP;
-			case LINE:
-				if (pos == Pos.FL || pos == Pos.FR || pos == Pos.BL || pos == Pos.BR){
-					return States.STOP;
-				}
-				if (pos == Pos.FM || pos == Pos.BM){
-					if (iteration == 1 || iteration == 3){
-						return States.MOVE;
-					}else{
-						return States.STOP;
-					}
-				}
-				break;
-			case SHOOT:
-				if (pos == Pos.FL || pos == Pos.FM || pos == Pos.BR || pos == Pos.BM){
-					if (iteration == 1 || iteration == 3 || iteration == 5){
-						return States.MOVE;
-					}
-				}
-				if (pos == Pos.FR || pos == Pos.BL){
-					if (iteration == 2){
-						return States.MOVE;
-					}
-				}
-				break;
-			case GEAR:
-				if (pos == Pos.FL || pos == Pos.FR || pos == Pos.BL || pos == Pos.BR){
-					if (iteration == 2){
-						return States.MOVE;
-					}
-				}else if(pos == Pos.FM || pos == Pos.BM){
-						return States.STOP;
-				}
-				break;
-			default:
-				return States.STOP;	
-		}
-		return States.STOP;
+	public void setValue(double value) {
+		desired = current + value;
 	}
+	
+	@Override
+	public void run() {
+		System.out.println("turning");
+		while (!inRange()){
+			drive.autoAngle(current, desired);
+		}
+		drive.stop();
+		desired = 400;
+		
+	}
+
 }
 	
