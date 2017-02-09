8069987ad7fe0fc7d025b92d6c6b007b2741acdd
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-09 09:55:06 -0800

    renamed Target to SteamWorksTarget

--- 2017code/src/org/usfirst/frc/team3328/robot/networking/SteamWorksTarget.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/networking/SteamWorksTarget.java
@@ -1,44 +1,41 @@
 package org.usfirst.frc.team3328.robot.networking;
 
 
-public class Target {
+public class SteamWorksTarget implements Target {
 	//private AtomicReference<Double> pixel; 
 	private double pixel;
-	private double distance;
 	private long lastTime = 0, time = 0;
 	private boolean status = false;
 	
+	@Override
 	public double getPixel(){
 		//return pixel.get();
 		return pixel;
 	}
 	
+	@Override
 	public void setPixel(double ang){
 		//System.out.println("set pixel to " + ang);
 		//pixel.set(ang);
 		pixel = ang;
 	}
 	
-	public double getDistance(){
-		return distance;
-	}
-	
-	public void setDistance(double dist){
-		distance = dist;
-	}
-	
+	@Override
 	public void setStatus(boolean stat){
 		status = stat;
 	}
 	
+	@Override
 	public boolean getStatus(){
 		return status;
 	}
 	
+	@Override
 	public void setTime(long stamp){
 		time = stamp;
 	}
 	
+	@Override
 	public boolean isNew(){
 		if (time != lastTime){
 			lastTime = time;
@@ -48,6 +45,7 @@ public class Target {
 		}
 	}
 	
+	@Override
 	public void printValues(){
 		System.out.printf("Pixel: %06.2f\n", getPixel());
 	}
