3a74edb3f4cd46b57eaa93abd59a90b20a46f553
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-09 09:56:57 -0800

    removed dead code

--- 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksDriveSystem.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksDriveSystem.java
@@ -6,22 +6,27 @@ import org.usfirst.frc.team3328.robot.utilities.DriveTalons;
 import org.usfirst.frc.team3328.robot.utilities.SteamWorksXbox.Buttons;
 import org.usfirst.frc.team3328.robot.utilities.Tracking;
 
-public class SteamWorksDriveSystem implements DriveSystem {
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+public class SteamWorksDriveSystem extends PIDSubsystem implements DriveSystem {
 	
 	Controller driveXbox;
 	Tracking track;
 	private DriveTalons talons;
 	private DriveEncoders encoders;
-	private double restraint = 1;
-	private double factor = 1.1;
+	public double restraint = 1;
 	public double displacement;
-	private double angleDeadZone = 20;
+	private double angleDeadZone = 0;
 	private double angleSpeed = .08;
-	private double speed;
+	private double gearSpeed = .1;
+	boolean placingGear = false;
 	
 	//instantiates talons assigns controller to "con"
 	public SteamWorksDriveSystem(DriveEncoders driveEncoders, DriveTalons driveTalons
 								, Controller Controller1, Tracking PID){
+		super("anglePID", 0, 0, 0);
+		setAbsoluteTolerance(0.05);
+		getPIDController().setContinuous(false);
 		encoders = driveEncoders;
 		talons = driveTalons;
 		driveXbox = Controller1;
@@ -29,22 +34,11 @@ public class SteamWorksDriveSystem implements DriveSystem {
 		track.setGoal(320);
 	}
 	
-	@Override
-	public double getDistance(){
-		return (encoders.frDistance() + encoders.blDistance()) / 2;
-	}
-	
 	public void resetDistance(){
 		encoders.reset();
 	}
 	
-	@Override
-	public double getSpeed(){
-		speed = ((driveXbox.getX() + driveXbox.getY()) / restraint) * 10;
-		speed = (speed * speed) / 100;
-		return speed;
-	}
-	
+
 	@Override
 	//moves the robot
 	public void move(double left, double right){
@@ -67,7 +61,8 @@ public class SteamWorksDriveSystem implements DriveSystem {
 	//the speed during teleop is divided by "restraint"
 	//the left and right bumpers lower and raise "restraint" respectively
 	//"restraint" is confined between 10 & 1
-	private void restrain(){
+	@Override
+	public void restrain(){
 		if (driveXbox.getButtonRelease(Buttons.LBUMP) && restraint > 1){
 			restraint -= 1;
 		}
@@ -76,28 +71,43 @@ public class SteamWorksDriveSystem implements DriveSystem {
 		}
 	}
 	
-	//gets displacement between the desired angle and the current angle
-	//normalizes displacement so it's between 0 - 1
-	//rounds displacement off to 2 decimal places
-	//all values above 0 and below .05 are set to .05
 	@Override
-	public void updateDisplacement(double desired, double current){
-		displacement = (current - desired) / 360;
-		if (displacement > 0 && displacement < .05){
-			displacement = .05;
+	protected double returnPIDInput() {
+		return displacement;
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		angleSpeed = output;
+	}
+	
+	//uses PID to determine the speed it turns
+	@Override
+	public void autoAngle(double current, double desired){
+		displacement = desired - current;
+		if (current > desired + angleDeadZone){
+			move(-angleSpeed, angleSpeed);
+		}else if(current < desired + angleDeadZone){
+			move(angleSpeed, -angleSpeed);
+		}else{
+			stop();
 		}
 	}
 	
-	//Uses the gyro to turn until it reaches a desired angle.
-	//should work while moving and while stopped
-	//the speed of each side is separately adjusted using the displacement
 	@Override
-	public void autoAngle(double current, double desired){
-		displacement = Math.abs(desired - current);
-		if (displacement > angleDeadZone){
-			talons.right(angleSpeed * factor);
-			talons.left(angleSpeed * factor);
+	public void placeGear(){
+		placingGear = true;
+		if(encoders.getDistance() > 1){
+			move(gearSpeed, gearSpeed);
+		}else{
+			placingGear = false;
+			stop();
 		}
+	}
+	
+	@Override
+	public boolean placingGear(){
+		return placingGear;
 	}
 	
 	//updates the value of "restraint"
@@ -112,6 +122,13 @@ public class SteamWorksDriveSystem implements DriveSystem {
 		}else{
 			move(track.track(), -track.track());
 		}
+	}
+
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
