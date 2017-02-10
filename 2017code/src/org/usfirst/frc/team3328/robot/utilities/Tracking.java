2a6c5f7ceecd41649a2a2b14f086c04105d34f60
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-10 13:49:35 -0800

    removed PID and controller

--- 2017code/src/org/usfirst/frc/team3328/robot/utilities/Tracking.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/utilities/Tracking.java
@@ -1,26 +1,21 @@
 package org.usfirst.frc.team3328.robot.utilities;
 
 import org.usfirst.frc.team3328.robot.networking.Target;
-import org.usfirst.frc.team3328.robot.utilities.SteamWorksXbox.Buttons;
 
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
-
-public class Tracking extends PIDSubsystem{
+public class Tracking {
 	
 	Target target;
 	Controller utilXbox;
+	PID pid;
 	private double pixel;
 	private double trackSpeed;
 	private double goal;
 	private int deadZone = 10;
 	private boolean tracking;
 	
-	public Tracking(Target pixelTarget, Controller utilitiesController){
-		super("tracking", 0, 0, 0);
-		setAbsoluteTolerance(0.05);
-		getPIDController().setContinuous(false);
-		target = pixelTarget;
-		utilXbox = utilitiesController;
+	public Tracking(Target target, PID pid){
+		this.target = target;
+		this.pid = pid;
 	}
 	
 	public void setGoal(int target){
@@ -35,25 +30,26 @@ public class Tracking extends PIDSubsystem{
 		return tracking;
 	}
 	
-	//toggles tracking if the left bumper is pressed
-	//disables tracking if aligned
-	//returns true if tracking
+	public void toggleTracking(){
+		tracking = !tracking;
+	}
+	
 	public boolean isTracking(){
 		if (pixel < goal + deadZone && pixel > goal - deadZone){
 			tracking = false;
 		}
-		if (utilXbox.getButtonRelease(Buttons.LBUMP)){
-			tracking = !tracking;
-		}
 		return tracking;
 	}	
 	
-	//takes a pixel offset to aim the robot
-	//turns until it's within a specified dead zone
-	//turns at trackSpeed if outside of dead zone
+	public void updateTrackSpeed(){
+		pid.setError(pixel - goal);
+		trackSpeed = pid.getCorrection();
+	}
+	
 	public double track(){
 		pixel = target.getPixel();
 		tracking = true;
+		updateTrackSpeed();
 		if (pixel > goal + deadZone){
 			return trackSpeed;
 		}else if (pixel < goal - deadZone){
@@ -62,28 +58,5 @@ public class Tracking extends PIDSubsystem{
 			return 0;
 		}
 	}
-
-
-	@Override
-	protected double returnPIDInput() {
-		return (pixel - goal);
-	}
-
-
-	@Override
-	protected void usePIDOutput(double output) {
-		trackSpeed = output;
-		
-	}
-
-
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-	
 	
 }
