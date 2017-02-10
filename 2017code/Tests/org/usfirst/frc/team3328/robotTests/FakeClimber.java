c64551c061d1436d509a0a34f180216a29de3676
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-10 13:33:42 -0800

    updated controlClimber method to take a double

--- 2017code/Tests/org/usfirst/frc/team3328/robotTests/FakeClimber.java
+++ 2017code/Tests/org/usfirst/frc/team3328/robotTests/FakeClimber.java
@@ -1,11 +1,11 @@
-package org.usfirst.frc.team3328.robot;
+package org.usfirst.frc.team3328.robotTests;
 
 import org.usfirst.frc.team3328.robot.subsystems.Climber;
 
 public class FakeClimber implements Climber {
 
 	@Override
-	public void controlClimber() {
+	public void controlClimber(double xAxis) {
 		// TODO Auto-generated method stub
 
 	}
