3423fa2eeea274ce40e2cc7929f796081ae0c9fb
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-10 13:44:30 -0800

    no longer takes a controller object

--- 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksClimber.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksClimber.java
@@ -1,32 +1,26 @@
 package org.usfirst.frc.team3328.robot.subsystems;
 
-import org.usfirst.frc.team3328.robot.utilities.Controller;
-
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class SteamWorksClimber implements Climber {
 	
-	Controller con;
 	SpeedController winch;
 	boolean active = false;
 	double factor = 5;
 	
-	public SteamWorksClimber(SpeedController speedController, Controller controller){
-		winch = speedController;
-		con = controller;
+	public SteamWorksClimber(SpeedController winch){
+		this.winch = winch;
 	}
 	
 	private void move(double speed){
 		winch.set(speed / factor);
 	}
 	
-	//Sets the climber to the xbox x-axis
-	// x-axis = right trigger - left trigger
 	@Override
-	public void controlClimber(){
-			if (con.getX() > 0){
-				move(con.getX());
-			} 
+	public void controlClimber(double xAxis){
+		if (xAxis > 0){
+			move(xAxis);
+		} 
 	}
 	
 }
