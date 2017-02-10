24e99bacdce873fc84e0947649a8e97ba5478d6d
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-10 13:45:28 -0800

    no longer takes a controller object

--- 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksFeeder.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksFeeder.java
@@ -1,32 +1,26 @@
 package org.usfirst.frc.team3328.robot.subsystems;
 
-import org.usfirst.frc.team3328.robot.utilities.Controller;
-import org.usfirst.frc.team3328.robot.utilities.SteamWorksXbox.Buttons;
 
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class SteamWorksFeeder implements Feeder{ 
 
 	SpeedController feeder;
-	Controller xbox;
 	boolean active = false;
 
-	public SteamWorksFeeder(SpeedController talonController, Controller xboxController){ 
-		feeder = talonController;
-		xbox = xboxController;	
+	public SteamWorksFeeder(SpeedController feeder){ 
+		this.feeder = feeder;
 	}
 
 	//Toggles feeder on and off when A and B is pressed
 	@Override
 	public void controlFeeder() {
-		if (xbox.getButtonRelease(Buttons.A) || xbox.getButtonRelease(Buttons.B)){
-			if (active) {
-				feeder.set(0);
-			}else{
-				feeder.set(1);
-			}
-			active = !active;
+		if (active) {
+			feeder.set(0);
+		}else{
+			feeder.set(1);
 		}
+		active = !active;
 	}
 	
 	
