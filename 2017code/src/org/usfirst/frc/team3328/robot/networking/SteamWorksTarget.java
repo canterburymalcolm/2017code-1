322f0526f372626def023caf31d0b9051360add1
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-17 12:27:51 -0800

    added getdistance and set distance methods

--- 2017code/src/org/usfirst/frc/team3328/robot/networking/SteamWorksTarget.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/networking/SteamWorksTarget.java
@@ -4,8 +4,15 @@ package org.usfirst.frc.team3328.robot.networking;
 public class SteamWorksTarget implements Target {
 	//private AtomicReference<Double> pixel; 
 	private double pixel;
-	private long lastTime = 0, time = 0;
+	private double distance;
 	private boolean status = false;
+	
+	@Override
+	public void setPixel(double pixel){
+		//System.out.println("set pixel to " + ang);
+		//pixel.set(ang);
+		this.pixel = pixel;
+	}
 	
 	@Override
 	public double getPixel(){
@@ -14,10 +21,13 @@ public class SteamWorksTarget implements Target {
 	}
 	
 	@Override
-	public void setPixel(double pixel){
-		//System.out.println("set pixel to " + ang);
-		//pixel.set(ang);
-		this.pixel = pixel;
+	public void setDistance(double distance){
+		this.distance = distance;
+	}
+	
+	@Override
+	public double getDistance(){
+		return distance;
 	}
 	
 	@Override
@@ -30,23 +40,9 @@ public class SteamWorksTarget implements Target {
 		return status;
 	}
 	
-	@Override
-	public void setTime(long time){
-		this.time = time;
-	}
-	
-	@Override
-	public boolean isNew(){
-		if (time != lastTime){
-			lastTime = time;
-			return true;
-		}else{
-			return false;
-		}
-	}
 	
 	@Override
 	public void printValues(){
-		System.out.printf("Pixel: %06.2f|Status: %b\n", getPixel(), getStatus());
+		System.out.printf("Pixel: %06.2f| Distance: %06.2f| Status: %b\n", getPixel(), getDistance(), getStatus());
 	}
 }
--- 2017code/src/org/usfirst/frc/team3328/robot/networking/Target.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/networking/Target.java
@@ -2,17 +2,17 @@ package org.usfirst.frc.team3328.robot.networking;
 
 public interface Target {
 
-	double getPixel();
-
 	void setPixel(double ang);
+	
+	double getPixel();
+	
+	void setDistance(double distance);
+
+	double getDistance();
 
 	void setStatus(boolean stat);
 
 	boolean getStatus();
-
-	void setTime(long stamp);
-
-	boolean isNew();
 
 	void printValues();
 
