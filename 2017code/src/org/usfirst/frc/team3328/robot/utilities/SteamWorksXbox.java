2b36510d1a608b7fcd255808053507bf32c8f7cb
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-16 21:25:08 -0800

    added buttons enum

--- 2017code/src/org/usfirst/frc/team3328/robot/utilities/SteamWorksXbox.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/utilities/SteamWorksXbox.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.XboxController;
 public class SteamWorksXbox implements Controller {
 	
 	XboxController xbox;
-	//array to store previous value of buttons
+
 	boolean[] button = new boolean[10];
 	
 	public enum Buttons {A(1), B(2), X(3), Y(4), LBUMP(5), RBUMP(6); 
@@ -15,27 +15,23 @@ public class SteamWorksXbox implements Controller {
 		}
 	}
 	
-	//instantiates an xbox controller
 	public SteamWorksXbox(int channel){
 		xbox = new XboxController(channel);
 	}
 	
-	//returns the right trigger minus the left trigger to allow for forwards and reverse
 	@Override
 	public double getX(){
 		return xbox.getRawAxis(3) - xbox.getRawAxis(2);
 	}
 	
-	//returns the x axis with a deadband of .1
 	@Override
 	public double getY(){
-		if (Math.abs(xbox.getRawAxis(0)) > .1){
+		if (Math.abs(xbox.getRawAxis(0)) > .15){
 			return xbox.getRawAxis(0);
 		}
 		return 0;
 	}
 	
-	//returns true when the button with the index "num" has been released
 	@Override
 	public boolean getButtonRelease(Buttons but){
 		int num = but.value;
@@ -47,15 +43,12 @@ public class SteamWorksXbox implements Controller {
 		return false;
 	}
 	
-	//returns true while the button is pressed not when released
 	@Override
 	public boolean getButtonPress(Buttons but){
 		int num = but.value;
 		return xbox.getRawButton(num);
 	}
 	
-	//returns Dpad values
-	//0 up 180 down clockwise increasing
 	public double getPad(){
 		return xbox.getPOV();
 	}
