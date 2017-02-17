fa05c23bb6063bfb4d18ef6b20279f8868b4eb23
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-16 20:16:35 -0800

    added autocode to test Periodic to fine tune the pid constants

--- 2017code/src/org/usfirst/frc/team3328/robot/Robot.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/Robot.java
@@ -7,59 +7,66 @@ import org.usfirst.frc.team3328.robot.subsystems.SteamWorksFeeder;
 import org.usfirst.frc.team3328.robot.subsystems.SteamWorksHotelLobby;
 import org.usfirst.frc.team3328.robot.subsystems.SteamWorksShooter;
 import org.usfirst.frc.team3328.robot.utilities.ADIS16448_IMU;
+import org.usfirst.frc.team3328.robot.utilities.Controller;
 import org.usfirst.frc.team3328.robot.utilities.DriveEncoders;
 import org.usfirst.frc.team3328.robot.utilities.DriveTalons;
-import org.usfirst.frc.team3328.robot.utilities.PID;
+import org.usfirst.frc.team3328.robot.utilities.PIDTest;
 import org.usfirst.frc.team3328.robot.utilities.REVDigitBoard;
 import org.usfirst.frc.team3328.robot.utilities.ShooterTalons;
 import org.usfirst.frc.team3328.robot.utilities.SteamWorksXbox;
 import org.usfirst.frc.team3328.robot.utilities.Tracking;
+import org.usfirst.frc.team3328.robot.utilities.SteamWorksXbox.Buttons;
+
+import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Talon;
-
 import states.StateMachine;
 
 public class Robot extends IterativeRobot {
 	Teleop telop;
 	StateMachine auto;
-	REVDigitBoard digit;
-	NetworkTablesTargetProvider targetProvider;
+	REVDigitBoard digit;  
+	Controller xbox;
+	PIDTest pid;
+	boolean autoActive = false;
+
 
 	@Override
 	public void robotInit() {
+		xbox = new SteamWorksXbox(1);
+		pid = new PIDTest(0 ,0, 0);
 		telop = new Teleop(
 				new SteamWorksDriveSystem(
 					new DriveEncoders(
-						new Encoder(2,3), 
-						new Encoder(4,5)),
+						new Encoder(0,1),
+						new Encoder(2,3)),
 					new DriveTalons(
 						new Talon(0),
 						new Talon(1),
 						new Talon(2),
 						new Talon(3)),
 					new Tracking(
-							targetProvider.getTarget(),
-							new PID("track",0 ,0 ,0)),
+						new NetworkTablesTargetProvider().getTarget(),
+						new PIDTest(.5 ,.05 ,0)),
 					new ADIS16448_IMU(),
-					new PID("autoAngle",0 ,0 ,0)),
+					pid),
 				new SteamWorksShooter(
-						new Encoder(0,1),
-						new ShooterTalons(
-							new Talon(6),
-							new Talon(7)),
+					new Encoder(4,5),
+					new ShooterTalons(
+						new CANTalon(0),  
+						new CANTalon(1)),
 						new SteamWorksHotelLobby(
-							new Talon(8))),
+						new CANTalon(2))),
 				new SteamWorksFeeder(
-						new Talon(5)),
+					new CANTalon(3)),
 				new SteamWorksClimber(
-						new Talon(4)),
-				new SteamWorksXbox(0),
-				new SteamWorksXbox(1));
-		auto = new StateMachine(2, telop);
+					new Talon(4)),
+				xbox, //util
+				new SteamWorksXbox(0)); //drive
+		auto = new StateMachine(20, telop);
 		digit = new REVDigitBoard();
-		targetProvider = new NetworkTablesTargetProvider();
 	}
 
 	@Override
@@ -79,5 +86,40 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void testPeriodic() {
+		if (xbox.getButtonRelease(Buttons.A)){
+			autoActive = !autoActive;
+		}
+		if (xbox.getButtonPress(Buttons.LBUMP)){
+			if (xbox.getButtonRelease(Buttons.X)){
+				pid.adjustP(-.02);
+				System.out.printf("P: %05.2f|I: %05.2f|D: %05.2f\n", pid.getP(), pid.getI(), pid.getD());
+			}
+			if (xbox.getButtonRelease(Buttons.Y)){
+				pid.adjustI(-.01);
+				System.out.printf("P: %05.2f|I: %05.2f|D: %05.2f\n", pid.getP(), pid.getI(), pid.getD());
+			}
+			if (xbox.getButtonRelease(Buttons.B)){
+				pid.adjustD(-.02);
+				System.out.printf("P: %05.2f|I: %05.2f|D: %05.2f\n", pid.getP(), pid.getI(), pid.getD());
+			}
+		}else{
+			if (xbox.getButtonRelease(Buttons.X)){
+				pid.adjustP(.02);
+				System.out.printf("P: %05.2f|I: %05.2f|D: %05.2f\n", pid.getP(), pid.getI(), pid.getD());
+			}
+			if (xbox.getButtonRelease(Buttons.Y)){
+				pid.adjustI(.01);
+				System.out.printf("P: %05.2f|I: %05.2f|D: %05.2f\n", pid.getP(), pid.getI(), pid.getD());
+			}
+			if (xbox.getButtonRelease(Buttons.B)){
+				pid.adjustD(.02);
+				System.out.printf("P: %05.2f|I: %05.2f|D: %05.2f\n", pid.getP(), pid.getI(), pid.getD());
+			}
+		}
+		if (autoActive){
+			auto.run();
+		}else{
+			auto.reset();
+		}
 	}
 }
