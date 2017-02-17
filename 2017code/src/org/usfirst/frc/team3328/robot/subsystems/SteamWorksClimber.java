102bd244f93a598006c77b55162c2f7970b6f588
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-16 20:16:10 -0800

    removed the handicap so it can go full speed now

--- 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksClimber.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksClimber.java
@@ -6,21 +6,22 @@ public class SteamWorksClimber implements Climber {
 	
 	SpeedController winch;
 	boolean active = false;
-	double factor = 5;
 	
 	public SteamWorksClimber(SpeedController winch){
 		this.winch = winch;
 	}
 	
 	private void move(double speed){
-		winch.set(speed / factor);
+		winch.set(speed);
 	}
 	
 	@Override
 	public void controlClimber(double xAxis){
 		if (xAxis > 0){
 			move(xAxis);
-		} 
+		}else{
+			move(0);
+		}
 	}
 	
 }
