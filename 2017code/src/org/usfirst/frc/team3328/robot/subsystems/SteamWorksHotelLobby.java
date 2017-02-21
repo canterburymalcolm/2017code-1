697667e8fe4af70a488dc9e3224f4d9455069202
Anton Panis <aopanis@gmail.com>
2017-02-21 13:55:52 -0800

    updated toggle method

--- 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksHotelLobby.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksHotelLobby.java
@@ -5,14 +5,20 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class SteamWorksHotelLobby implements HotelLobby {
 	
 	SpeedController belt;
+	double beltSpeed = -.5;
 	
 	public SteamWorksHotelLobby(SpeedController belt) {
 		this.belt = belt;
 	}
 	
 	@Override
+	public void toggle(){
+		belt.set((belt.get() == 0) ? beltSpeed : 0);
+	}
+	
+	@Override
 	public void runBelt(){
-		belt.set(1);
+		belt.set(beltSpeed);
 	}
 	
 	@Override
