41214a6d34077e40856acd33d498bc4c521a8be1
Anton Panis <aopanis@gmail.com>
2017-02-21 13:56:28 -0800

    updated toggling method

--- 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksFeeder.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksFeeder.java
@@ -6,21 +6,14 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class SteamWorksFeeder implements Feeder{ 
 
 	SpeedController feeder;
-	boolean active = false;
 
 	public SteamWorksFeeder(SpeedController feeder){ 
 		this.feeder = feeder;
 	}
 
-	//Toggles feeder on and off when A and B is pressed
 	@Override
 	public void controlFeeder() {
-		if (active) {
-			feeder.set(0);
-		}else{
-			feeder.set(1);
-		}
-		active = !active;
+		feeder.set((feeder.get() == 0) ? 1 : 0);
 	}
 	
 	
