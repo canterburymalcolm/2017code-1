ea81a17f5bc02967849dc6ba19fd744aefd7b9c5
Anton Panis <aopanis@gmail.com>
2017-03-02 17:52:22 -0800

    added voltage ramp rate functionality

--- 2017code/src/org/usfirst/frc/team3328/robot/utilities/ShooterTalons.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/utilities/ShooterTalons.java
@@ -1,17 +1,20 @@
 package org.usfirst.frc.team3328.robot.utilities;
 
-import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.CANSpeedController;
 
 public class ShooterTalons {
 	
-	private SpeedController t1;
-	private SpeedController t2;
+	private CANSpeedController t1;
+	private CANSpeedController t2;
+	double rampRate = 6;
 	
-	public ShooterTalons(SpeedController t1, SpeedController t2){
+	public ShooterTalons(CANSpeedController t1, CANSpeedController t2){
 		this.t1 = t1;
 		this.t2 = t2;
+		this.t1.setVoltageRampRate(rampRate);
+		this.t2.setVoltageRampRate(rampRate);
 	}
-
+	
 	public void set(double speed){
 		t1.set(speed);
 		t2.set(speed);
