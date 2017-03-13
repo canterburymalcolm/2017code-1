23f617c846c5da17d68a64ecc4733ece22584587
Anton Panis <aopanis@gmail.com>
2017-03-13 15:32:47 -0700

    adjusted extending code and angle of travel

--- 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksArm.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksArm.java
@@ -1,29 +1,37 @@
 package org.usfirst.frc.team3328.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SteamWorksArm implements Arm{
 	
 	Servo servo;
+	double startAngle = 0;
+	boolean isOpen = false;
 	
 	public SteamWorksArm(Servo servo){
 		this.servo = servo;
+		this.servo.setAngle(startAngle);
+		SmartDashboard.putBoolean("Door Open", isOpen);
 	}
 	
 	@Override
 	public void extend() {
-		servo.set(50);
-		
+		servo.setAngle(100);
+		isOpen = true;
+		SmartDashboard.putBoolean("Door Open", isOpen);
 	}
 
 	@Override
 	public void rectract() {
-		servo.set(0);
+		servo.setAngle(startAngle);
+		isOpen = false;
+		SmartDashboard.putBoolean("Door Open", isOpen);
 	}
 
 	@Override
 	public boolean isExtended() {
-		return servo.get() > 0;
+		return servo.get() > startAngle;
 	}
 
 	
